486f711b1e465f5a76cdd4b63db06aa54d6c4a9c
Mustapha-Aisha <Mustaphaaisha622@gmail.com>
2025-04-06 15:24:46 +0100

    init

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { UserResolver } from './user/user.resolver';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService, UserResolver],
+})
+
+
+export class AppModule {}
